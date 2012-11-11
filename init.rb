5547af52248f2c43de2794f1119248e46e75e955
Mark McGranaghan <mmcgrana@gmail.com>
2012-11-11 08:59:37 -0800

    workable with curl

--- init.rb
+++ init.rb
@@ -1,9 +1,16 @@
 require "heroku/command/base"
 
 class Heroku::Command::Sightglass < Heroku::Command::Base
-  
+
   # sightglass:logs
+  #
+  # display logs for an app
   def logs
-    puts("logs logs logs!")
+    config = api.get_config_vars(app).body
+    if url = config["SIGHTGLASS_URL"]
+      puts(url)
+    else
+      error("Sightglass is not installed on this app.\nInstall it with `heroku addons:add sightglass -a #{app}`.")
+    end
   end
 end
